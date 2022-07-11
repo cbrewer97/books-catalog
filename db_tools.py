1448dd17de9b4e1ebe1451217e0cc38575b7b3d2
LAPTOP-91PLOLFO\books <booksmart00@yahoo.com>
2022-07-11 09:15:40 -0500

    Added function create_books_table()
    Added function is_integer()
      - Helps with datatype logic for search function
    
    Added function search_books()

--- db_tools.py
+++ db_tools.py
@@ -14,6 +14,7 @@ if exists('books.db'):
     print('books.db found. Connected to db.')
 else:
     connection=sqlite3.connect('books.db')
+    print('Created file books.db ')
     
 def get_column_names(table_name):
     global connection
@@ -24,6 +25,7 @@ def get_column_names(table_name):
     return names
 
 def get_tables():
+    global connection
     results=connection.execute("select name from sqlite_master where type='table';")
     tables=results.fetchall()
     return tables
@@ -35,7 +37,7 @@ def insert_record(**kwargs):
         if key not in get_column_names('books'):
             print('Invalid key: '+key)
             print('Please use a valid key from the following list: ' +str(get_column_names('books')) )
-            raise Exception('Invalid key. Please use keys from the following list :'+ str(get_column_names('books')) )
+            raise Exception('Invalid key. Please use keys from the following list: '+ str(get_column_names('books')) )
         else:
             values_dict[key]=value
     keys_tuple=tuple(values_dict.keys())
@@ -49,4 +51,36 @@ def insert_record(**kwargs):
     query_string="insert into books"+keys_string+" values"+values_string
     print(query_string)
     connection.execute(query_string)
+    connection.commit()
     
+def create_books_table():
+    global connection
+    connection.execute('create table books(id integer primary key, \
+        title text, authors text, publisher text, publish_date text, \
+        description text, isbn10 integer, isbn13 integer, \
+        page_count integer, image_path text )')
+    connection.commit()
+        
+def is_integer(value):
+    flag=False
+    if type(value)==type('string'):
+        flag=value.isdigit()
+    elif type(value)==type(1):
+        flag=True
+    return flag
+
+def search_books(**kwargs):
+    query_string="select * from books"
+    if len(kwargs)>0:
+        query_string=query_string+" where "
+    for key, value in kwargs.items():
+        if key not in get_column_names('books'):
+            raise Exception('Invalid key. Please use keys from the following list: '+ str(get_column_names('books')) )
+        elif is_integer(value):
+            query_string=query_string+key+"="+str(value)+" and "
+        elif type(value)==type('string'):
+            query_string=query_string+key+" like '%"+value+"%' and "
+    query_string=query_string.strip("and ")
+    return connection.execute(query_string).fetchall()
+            
+            
