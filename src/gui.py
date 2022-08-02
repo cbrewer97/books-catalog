e15ed919ae57d6744344b4d35e59876fdde2d1f7
LAPTOP-91PLOLFO\books <booksmart00@yahoo.com>
2022-08-02 10:11:24 -0500

    Moved search_button and clear_button to SearchFrame class definition
    Reordered code to have all class and function definitions first, followed by the execution code

--- src/gui.py
+++ src/gui.py
@@ -32,23 +32,7 @@ from tkscrolledframe import ScrolledFrame
 #
 #
 
-root=tk.Tk()
 
-main_frame=ttk.Frame(root)
-main_frame.pack(fill='both', expand=True)
-#main_frame.grid(sticky='nsew')
-#main_frame.grid_propagate(0)
-
-
-navigation_frame=ttk.Frame(main_frame)
-navigation_frame.grid(row=0, sticky='nsew')
-
-
-my_books_button=ttk.Button(navigation_frame, text="My Books")
-my_books_button.grid(row=1,column=2)
-
-google_search_button=ttk.Button(navigation_frame, text="Google Books")
-google_search_button.grid(row=1, column=3)
 
 
 
@@ -101,10 +85,16 @@ class SearchFrame:
         for index, field in enumerate([title_search_field, author_search_field, date_search_field, isbn_search_field, tags_search_field]):
             field.grid(row=index, column=1, sticky='w')
             
-        print(self.frame.winfo_children())
-    
-search_frame=SearchFrame(main_frame)
-print(search_frame.frame.grid_size())
+        self.search_button=ttk.Button(self.frame, text="Search", \
+                    command=lambda:populate_result_frames(\
+                                title=self.text_vars['title'].get(), \
+                                author=self.text_vars['author'].get()))
+        self.search_button.grid(row=self.frame.grid_size()[1], column=1, sticky='e')
+
+        self.clear_button=ttk.Button(self.frame, text='Clear', command=clear_books_results)
+        self.clear_button.grid(row=self.frame.grid_size()[1]-1, column=0)
+        
+
     
 
 class BookFrame():
@@ -122,15 +112,41 @@ class BookFrame():
         self.frame.grid_columnconfigure(2, weight=2)
 
 
-
-
-scrolled_frame=ScrolledFrame(main_frame, scrollbars='vertical',use_ttk=True )
-scrolled_frame.grid(row=2, sticky='ns')
-scrolled_frame.bind_arrow_keys(root)
-scrolled_frame.bind_scroll_wheel(root)
-my_books_frame=scrolled_frame.display_widget(tk.Frame,fit_width=True)
-my_books_frame['borderwidth']=5
-my_books_frame['relief']='sunken'
+class MyBooksFrame():
+    def __init__(self, main_frame):
+        scrolled_frame=ScrolledFrame(main_frame, scrollbars='vertical',use_ttk=True )
+        scrolled_frame.grid(row=2, sticky='ns')
+        scrolled_frame.bind_arrow_keys(root)
+        scrolled_frame.bind_scroll_wheel(root)
+        widget_class=ttk.Frame
+        books_frame=scrolled_frame.display_widget(widget_class,fit_width=True)
+        self.frame=books_frame
+        self.frame['borderwidth']=5
+        self.frame['relief']='sunken'
+        
+def populate_result_frames(title, author):
+    #title=title_text_variable.get()
+    search_results=dbt.search_books(title=title, authors=author)
+    
+    for result in search_results:
+        book_dict=dbt.as_dict(result)
+        result_frame=BookFrame(book_dict, my_books_frame.frame).frame
+        result_frame['borderwidth']=5
+        result_frame['relief']='sunken'
+        grid_size=my_books_frame.frame.grid_size()
+        print(grid_size)
+        result_frame.grid(row=grid_size[1]+1, column=0, sticky='w')
+        #result_frame.pack(fill='x',expand=True ,side='top')
+        # result_frame.grid(row=row_number,column=1,sticky='ew')
+        # result_frame.grid_columnconfigure(0, weight=1)
+        # result_frame.grid_rowconfigure(0 ,weight=1)
+        #row_number+=1
+        
+def clear_books_results():
+    children=my_books_frame.frame.winfo_children()
+    for child in children:
+        child.destroy()
+    my_books_frame.frame.grid_forget()
 
 
 #search_results=search_books()
@@ -142,38 +158,9 @@ my_books_frame['relief']='sunken'
 
 
 
-def populate_result_frames(title, author):
-    #title=title_text_variable.get()
-    search_results=dbt.search_books(title=title, authors=author)
-    
-    for result in search_results:
-        book_dict=dbt.as_dict(result)
-        result_frame=BookFrame(book_dict, my_books_frame).frame
-        result_frame['borderwidth']=5
-        result_frame['relief']='sunken'
-        grid_size=my_books_frame.grid_size()
-        print(grid_size)
-        result_frame.grid(row=grid_size[1]+1, column=0, sticky='w')
-        #result_frame.pack(fill='x',expand=True ,side='top')
-        # result_frame.grid(row=row_number,column=1,sticky='ew')
-        # result_frame.grid_columnconfigure(0, weight=1)
-        # result_frame.grid_rowconfigure(0 ,weight=1)
-        #row_number+=1
-        
-def clear_books_results():
-    children=my_books_frame.winfo_children()
-    for child in children:
-        child.destroy()
-    my_books_frame.grid_forget()
-        
-search_button=ttk.Button(search_frame.frame, text="Search", \
-            command=lambda:populate_result_frames(\
-                        title=search_frame.text_vars['title'].get(), \
-                        author=search_frame.text_vars['author'].get()))
-search_button.grid(row=search_frame.frame.grid_size()[1], column=1, sticky='e')
 
-clear_button=ttk.Button(search_frame.frame, text='Clear', command=clear_books_results)
-clear_button.grid(row=search_frame.frame.grid_size()[1]-1, column=0)
+        
+
 
 # for result in search_results:
 #     book_dict=as_dict(result)
@@ -188,6 +175,28 @@ clear_button.grid(row=search_frame.frame.grid_size()[1]-1, column=0)
 #     # result_frame.grid_rowconfigure(0 ,weight=1)
 #     row_number+=1
 
+root=tk.Tk()
+
+main_frame=ttk.Frame(root)
+main_frame.pack(fill='both', expand=True)
+#main_frame.grid(sticky='nsew')
+#main_frame.grid_propagate(0)
+
+
+navigation_frame=ttk.Frame(main_frame)
+navigation_frame.grid(row=0, sticky='nsew')
+
+
+my_books_button=ttk.Button(navigation_frame, text="My Books")
+my_books_button.grid(row=1,column=2)
+
+google_search_button=ttk.Button(navigation_frame, text="Google Books")
+google_search_button.grid(row=1, column=3)
+
+search_frame=SearchFrame(main_frame)
+print(search_frame.frame.grid_size())
+
+my_books_frame=MyBooksFrame(main_frame)
 
 main_frame.grid_rowconfigure((0,1), weight=0)
 main_frame.grid_rowconfigure(2, weight=1)
